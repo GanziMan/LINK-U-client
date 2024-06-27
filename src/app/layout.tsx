acd43a5d4d678475186aef5c679daf469cd6b11f
GanziMan <qjatn50089@gmail.com>
2024-06-27 22:01:17 +0900

    ADD: inivation Form ADD

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
+const KAKAO_API_KEY = "5199d1d25fdf685b1f8614de4564477e";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,6 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          type="text/javascript"
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_API_KEY}&autoload=false`}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
