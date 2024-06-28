50bfbdd84569644087011ef943cd99784fd54291
GanziMan <qjatn50089@gmail.com>
2024-06-28 20:05:13 +0900

    ADD: 카카오 공유하기 기능 추가

---
+++ src/components/ShareKakao.tsx
@@ -0,0 +1,63 @@
+import { Box, styled } from "@mui/material";
+import { useEffect } from "react";
+
+interface LinkType {
+  mobileWebUrl: string;
+  webUrl: string;
+}
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
+export default function ShareKakao({
+  title,
+  imageUrl,
+  link,
+}: {
+  title: string;
+  imageUrl: string;
+  link: LinkType;
+}) {
+  const handleShare = async () => {
+    await window.Kakao.Share.sendDefault({
+      objectType: "text",
+      text: "기본 템플릿으로 제공되는 텍스트 템플릿은 텍스트를 최대 200자까지 표시할 수 있습니다. 텍스트 템플릿은 텍스트 영역과 하나의 기본 버튼을 가집니다. 임의의 버튼을 설정할 수도 있습니다. 여러 장의 이미지, 프로필 정보 등 보다 확장된 형태의 카카오톡 공유는 다른 템플릿을 이용해 보낼 수 있습니다.",
+      link: {
+        mobileWebUrl: "https://developers.kakao.com",
+        webUrl: "https://developers.kakao.com",
+      },
+    });
+  };
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.Kakao) {
+      if (!window.Kakao.isInitialized) {
+        window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY!);
+      }
+    }
+  }, []);
+
+  return (
+    <ShareButton background="#FFE39B" onClick={handleShare}>
+      카카오톡 공유하기
+    </ShareButton>
+  );
+}
+
+const ShareButton = styled(Box)(({ background }: { background: string }) => {
+  return {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "rgb(70, 65, 61)",
+    background: background,
+    fontSize: "16px",
+    lineHeight: "26px",
+    width: 230,
+    height: 46,
+    cursor: "pointer",
+    borderRadius: "100px",
+    fontWeight: 600,
+  };
+});
