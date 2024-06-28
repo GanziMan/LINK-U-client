e6948fce5e2c2a0a919b1564f87532d72977b1f3
GanziMan <qjatn50089@gmail.com>
2024-06-28 20:04:22 +0900

    ADD: kakaoScript 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,18 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
-
 const inter = Inter({ subsets: ["latin"] });
-const KAKAO_API_KEY = "5199d1d25fdf685b1f8614de4564477e";
 
 export const metadata: Metadata = {
-  title: "호영이와 도현이의 결혼",
-  description: "호영이와 도현이의 결혼",
+  title: "모바일 청첩장",
+  description: "소중한 사람들을 초대합니다.",
 };
 
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -21,9 +24,16 @@ export default function RootLayout({
       <head>
         <Script
           type="text/javascript"
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_API_KEY}&autoload=false`}
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env
+            .NEXT_PUBLIC_KAKAO_API_KEY!}&autoload=false`}
+        />
+        <Script
+          type="text/javascript"
+          strategy="afterInteractive"
+          src={`https://developers.kakao.com/sdk/js/kakao.min.js`}
         />
       </head>
+
       <body className={inter.className}>{children}</body>
     </html>
   );
