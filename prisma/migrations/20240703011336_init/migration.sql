5b24889bee091be23844715fabef638aa2d828d6
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-07-04 18:22:27 +0900

    ADD: prisma schema 생성

---
+++ prisma/migrations/20240703011336_init/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `Users`;
+
+-- CreateTable
+CREATE TABLE `Oners` (
+    `id` INTEGER NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
