61e034445027e399b139b17be42536e82160e53e
GanziMan <qjatn50089@gmail.com>
2024-07-10 01:55:29 +0900

    Fix: schema수정

--- src/features/invitation/schema.ts
+++ src/features/invitation/schema.ts
@@ -1,19 +1,19 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 export const getCountSchema = z.object({
   id: z.string(),
-})
+});
 
 export const updateCountSchema = z.object({
   id: z.string(),
-})
+});
 
 export const getCommentSchema = z.object({
   // take:z.number(),
   cursor: z.number(),
-})
+});
 
 export const createCommentSchema = z.object({
   name: z.string(),
   comment: z.string(),
-})
+});
