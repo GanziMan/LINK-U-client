0bb3feb76df124b5331fd6d758ff001ac16d7e0f
GanziMan <qjatn50089@gmail.com>
2024-07-10 01:55:01 +0900

    Fix: InfiniteQuery Pagination 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,26 @@
-'use client'
+"use client";
 
-import CommonAccordion from '@/components/Accordion'
-import BackgroundMusic from '@/components/BackgroundMusic'
-import KakaoMap from '@/components/KakaoMap'
-import CommonSwiper from '@/components/Swiper'
-import { Box, CircularProgress } from '@mui/material'
-import JSConfetti from 'js-confetti'
-import Image from 'next/image'
-import { SnackbarProvider, enqueueSnackbar } from 'notistack'
-import ShareKakao from '@/components/ShareKakao'
-import { getCount } from '@/features/invitation/getCount'
-import { updateCount } from '@/features/invitation/updateCount'
-import InfiniteScroll from 'react-infinite-scroll-component'
+import CommonAccordion from "@/components/Accordion";
+import BackgroundMusic from "@/components/BackgroundMusic";
+import KakaoMap from "@/components/KakaoMap";
+import CommonSwiper from "@/components/Swiper";
+import { Box, CircularProgress } from "@mui/material";
+import JSConfetti from "js-confetti";
+import Image from "next/image";
+import { SnackbarProvider, enqueueSnackbar } from "notistack";
+import ShareKakao from "@/components/ShareKakao";
+import { getCount } from "@/features/invitation/getCount";
+import { updateCount } from "@/features/invitation/updateCount";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 import {
   useInfiniteQuery,
   useMutation,
   useQuery,
   useQueryClient,
-} from '@tanstack/react-query'
-import { getComments } from '@/features/invitation/getComments'
-import { toFormikValidationSchema } from 'zod-formik-adapter'
+} from "@tanstack/react-query";
+import { getComments } from "@/features/invitation/getComments";
+import { toFormikValidationSchema } from "zod-formik-adapter";
 import {
   AccountBox,
   AccountBoxText,
@@ -51,80 +51,86 @@ import {
   VisitorBox,
   WeddingImageText,
   WeddingImageWrapper,
-} from './components'
-import { createComment } from '@/features/invitation/createComment'
-import { Formik, Form, Field, ErrorMessage } from 'formik'
-import { createCommentSchema } from '@/features/invitation/schema'
-import { number } from 'zod'
+} from "./components";
+import { createComment } from "@/features/invitation/createComment";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { createCommentSchema } from "@/features/invitation/schema";
+import { number } from "zod";
+import PaginationComponent from "@/components/Pagination";
+import { useEffect, useState } from "react";
 
 interface CommentFormValues {
-  name: string
-  comment: string
+  name: string;
+  comment: string;
 }
 
 const initialValues: CommentFormValues = {
-  name: '',
-  comment: '',
-}
+  name: "",
+  comment: "",
+};
 
 export interface AccountInfoType {
-  position: string
-  name: string
-  accountInfo: string
+  position: string;
+  name: string;
+  accountInfo: string;
 }
 
 export interface CommentType {
-  name: string
-  comment: string
-  date: string
-  nextCursor: string
+  name: string;
+  comment: string;
+  date: string;
+  nextCursor: string;
 }
 const GroomAccountInfo: AccountInfoType[] = [
   {
-    position: '신랑',
-    name: '김호영',
-    accountInfo: '카카오뱅크 0000-11-000000',
+    position: "신랑",
+    name: "김호영",
+    accountInfo: "카카오뱅크 0000-11-000000",
   },
   {
-    position: '아버지',
-    name: '김명래',
-    accountInfo: '카카오뱅크 0000-11-000000',
+    position: "아버지",
+    name: "김명래",
+    accountInfo: "카카오뱅크 0000-11-000000",
   },
   {
-    position: '어머니',
-    name: 'undefined',
-    accountInfo: '카카오뱅크 0000-11-000000',
+    position: "어머니",
+    name: "undefined",
+    accountInfo: "카카오뱅크 0000-11-000000",
   },
-]
+];
 const BrideAccountInfo: AccountInfoType[] = [
   {
-    position: '신부',
-    name: '김도현',
-    accountInfo: '카카오뱅크 0000-11-000000',
+    position: "신부",
+    name: "김도현",
+    accountInfo: "카카오뱅크 0000-11-000000",
   },
   {
-    position: '아버지',
-    name: '김동욱',
-    accountInfo: '카카오뱅크 0000-11-000000',
+    position: "아버지",
+    name: "김동욱",
+    accountInfo: "카카오뱅크 0000-11-000000",
   },
   {
-    position: '어머니',
-    name: '김명숙',
-    accountInfo: '카카오뱅크 0000-11-000000',
+    position: "어머니",
+    name: "김명숙",
+    accountInfo: "카카오뱅크 0000-11-000000",
   },
-]
+];
 
 export default function Page() {
-  const queryClient = useQueryClient()
-  const jsConfetti = new JSConfetti()
+  const queryClient = useQueryClient();
+  const jsConfetti = new JSConfetti();
+
+  const itemsPerPage = 5;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   const { data: likeCount } = useQuery({
-    queryKey: ['like-count'],
+    queryKey: ["like-count"],
     queryFn: async () => {
-      const response = await getCount({ id: '1' })
-      return response?.data?.like_count
+      const response = await getCount({ id: "1" });
+      return response?.data?.like_count;
     },
-  })
+  });
 
   const {
     data: commentPage,
@@ -133,37 +139,44 @@ export default function Page() {
     isFetching,
     isFetchingNextPage,
   } = useInfiniteQuery({
-    queryKey: ['comment-page'],
+    queryKey: ["comment-page"],
     queryFn: ({ pageParam = 1 }) => getComments({ cursor: pageParam }),
     initialPageParam: 1,
     getNextPageParam: (lastPage, allPages) => {
-      return lastPage?.data?.nextCursor || null
+      return lastPage?.data?.nextCursor || null;
     },
     getPreviousPageParam: (firstPage, allPages) => {
-      return firstPage?.data?.nextCursor || null
+      return firstPage?.data?.nextCursor || null;
     },
-  })
+  });
 
-  const comments = commentPage?.pages.flatMap(
-    (page) => page?.data?.formattedData
-  )
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    page: number
+  ) => {
+    setCurrentPage(page);
+  };
+
+  const comments = commentPage?.pages[0]?.data?.comments || [];
+
+  console.log(commentPage);
 
   const { mutate: likeCountMutation } = useMutation({
-    mutationFn: async () => await updateCount({ id: '1' }),
+    mutationFn: async () => await updateCount({ id: "1" }),
     onSuccess: () => {
       queryClient.invalidateQueries({
-        queryKey: ['like-count'],
-      })
+        queryKey: ["like-count"],
+      });
 
       jsConfetti.addConfetti({
-        confettiColors: ['#CAB0FF'],
+        confettiColors: ["#CAB0FF"],
         confettiNumber: 500,
-      })
+      });
     },
     onError: () => {
-      enqueueSnackbar('좋아요 업데이트 오류', { variant: 'error' })
+      enqueueSnackbar("좋아요 업데이트 오류", { variant: "error" });
     },
-  })
+  });
 
   // const { mutate: commentMutation } = useMutation({
   //   mutationFn: createComment,
@@ -181,31 +194,38 @@ export default function Page() {
 
   const copyUrlToClipboard = async () => {
     try {
-      await navigator.clipboard.writeText(window.location.href)
-      enqueueSnackbar<'success'>('모바일 청첩장 URL이 복사되었습니다!', {
-        variant: 'success',
-      })
+      await navigator.clipboard.writeText(window.location.href);
+      enqueueSnackbar<"success">("모바일 청첩장 URL이 복사되었습니다!", {
+        variant: "success",
+      });
     } catch (err) {
-      console.error('URL 복사 실패:', err)
-      enqueueSnackbar<'error'>('URL 복사에 실패했습니다.', {
-        variant: 'error',
-      })
+      console.error("URL 복사 실패:", err);
+      enqueueSnackbar<"error">("URL 복사에 실패했습니다.", {
+        variant: "error",
+      });
     }
-  }
+  };
 
   const kakaoMap = () => {
-    const latitude = 37.759027
-    const longitude = 126.774992
-    const kakaoMapUrl = `https://map.kakao.com/link/map/${latitude},${longitude}`
-    window.open(kakaoMapUrl, '_blank')
-  }
+    const latitude = 37.759027;
+    const longitude = 126.774992;
+    const kakaoMapUrl = `https://map.kakao.com/link/map/${latitude},${longitude}`;
+    window.open(kakaoMapUrl, "_blank");
+  };
 
+  useEffect(() => {
+    if (commentPage) {
+      if (commentPage.pages[0]?.data?.totalPages !== totalPages) {
+        setTotalPages(commentPage.pages[0]?.data?.totalPages!);
+      }
+    }
+  }, [currentPage, fetchNextPage, commentPage]);
   return (
     <SnackbarProvider
       autoHideDuration={1000}
       anchorOrigin={{
-        horizontal: 'center',
-        vertical: 'top',
+        horizontal: "center",
+        vertical: "top",
       }}
     >
       <InvitaionContainer>
@@ -226,13 +246,13 @@ export default function Page() {
           </InviteText>
           25.02.15.SAT
           <Image
-            src={'/images/image/gay.jpeg'}
+            src={"/images/image/gay.jpeg"}
             alt=""
             width={328}
             height={328}
           />
           <Image
-            src={'/images/image/greeting.jpg'}
+            src={"/images/image/greeting.jpg"}
             alt=""
             width={128}
             height={128}
@@ -255,18 +275,18 @@ export default function Page() {
             2025년 2월 15일 토요일 오후 12시 파주 통돼지 바베큐집
           </LocationText>
           <Image
-            src={'/images/icons/champagne-icon.svg'}
+            src={"/images/icons/champagne-icon.svg"}
             alt=""
             width={50}
             height={50}
             style={{
-              marginLeft: 'auto',
+              marginLeft: "auto",
             }}
           />
           <CommonSwiper />
           <AccountBox>
             <Image
-              src={'/images/icons/dove-icon.svg'}
+              src={"/images/icons/dove-icon.svg"}
               alt=""
               width={80}
               height={70}
@@ -275,19 +295,19 @@ export default function Page() {
           </AccountBox>
           <CommonAccordion
             AccountInfo={GroomAccountInfo}
-            backgroundColor={'rgb(240, 243, 246)'}
+            backgroundColor={"rgb(240, 243, 246)"}
             gender="신랑측"
             genderImageUrl="/images/icons/groom-icon.svg"
           />
           <CommonAccordion
             AccountInfo={BrideAccountInfo}
-            backgroundColor={'rgb(255, 245, 218)'}
+            backgroundColor={"rgb(255, 245, 218)"}
             gender="신부측"
             genderImageUrl="/images/icons/bridge-icon.svg"
           />
-          <Box display={'flex'} flexDirection={'column'} alignItems={'center'}>
+          <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
             <Image
-              src={'/images/icons/location-icon.svg'}
+              src={"/images/icons/location-icon.svg"}
               alt=""
               width={80}
               height={70}
@@ -300,25 +320,25 @@ export default function Page() {
           <KakaoMapButton onClick={() => kakaoMap()}>
             카카오맵에서 보기
           </KakaoMapButton>
-          <Box display={'flex'} flexDirection={'column'} gap={'20px'}>
+          <Box display={"flex"} flexDirection={"column"} gap={"20px"}>
             <Box>
               <GuideBox>주차안내</GuideBox>
-              <Box color={'#594739'}> 파주 통돼지집 내 주차장 이용</Box>
-              <Box color={'#7A3D0C'}>
+              <Box color={"#594739"}> 파주 통돼지집 내 주차장 이용</Box>
+              <Box color={"#7A3D0C"}>
                 * 신랑/신부가 무료로 제공하는 발렛서비스를 이용하십시오.
               </Box>
             </Box>
             <hr />
             <Box>
               <GuideBox>지하철</GuideBox>
-              <Box color={'#594739'}>
+              <Box color={"#594739"}>
                 [3호선] 풍산역 2번,3번 출구에서 도보 10분
               </Box>
             </Box>
             <hr />
             <Box>
               <GuideBox>버스</GuideBox>
-              <Box color={'#594739'}>
+              <Box color={"#594739"}>
                 파주중학교 또는 일진공원 하차 후 도보 5분 - 간선 141번, 지선
                 2011번, 직행 3600번
               </Box>
@@ -329,8 +349,8 @@ export default function Page() {
               <HeartCountBox>{likeCount}</HeartCountBox>
               <Image
                 alt=""
-                src={'/images/icons/heart-icon.svg'}
-                style={{ cursor: 'pointer' }}
+                src={"/images/icons/heart-icon.svg"}
+                style={{ cursor: "pointer" }}
                 width={100}
                 height={100}
               />
@@ -339,8 +359,8 @@ export default function Page() {
               title=""
               imageUrl=""
               link={{
-                webUrl: '',
-                mobileWebUrl: '',
+                webUrl: "",
+                mobileWebUrl: "",
               }}
               likeCount={likeCount!}
             />
@@ -350,26 +370,30 @@ export default function Page() {
           </ShareBox>
           <VisitorBox>방명록</VisitorBox>
           <CommentContainer>
-            {comments ? (
-              <InfiniteScroll
-                dataLength={comments?.length}
-                next={fetchNextPage}
-                hasMore={hasNextPage}
-                loader={<CircularProgress color="primary" size={50} />}
-              >
-                {comments?.map((comment: any) => (
-                  <div key={comment.id}>
-                    <h5>{comment.name}</h5>
-                    <p>{comment.comment}</p>
-                    <small>{comment.date}</small>
-                  </div>
-                ))}
-              </InfiniteScroll>
-            ) : (
-              <CircularProgress color="primary" size={50} />
-            )}
+            {comments &&
+              comments?.map((comment: any) => (
+                <div key={comment.id}>
+                  <h5>{comment.name}</h5>
+                  <p>{comment.comment}</p>
+                  <small>{comment.date}</small>
+                </div>
+              ))}
+            {/* {hasNextPage && (
+              <Box onClick={() => fetchNextPage()}>
+                {isFetchingNextPage ? (
+                  <CircularProgress size={30} sx={{ margin: "0 auto" }} />
+                ) : (
+                  "더 보기"
+                )}
+              </Box>
+            )} */}
+            <PaginationComponent
+              count={totalPages}
+              page={currentPage}
+              onChange={handlePageChange}
+            />
           </CommentContainer>
-          <Box width={'100%'}>
+          <Box width={"100%"}>
             <Formik
               initialValues={initialValues}
               validationSchema={toFormikValidationSchema(createCommentSchema)}
@@ -389,14 +413,14 @@ export default function Page() {
                             height: 10,
                           },
                         }}
-                        {...getFieldProps('name')}
+                        {...getFieldProps("name")}
                       />
                     </CommentWriteNameBox>
                     <CommentWriteContentBox>
                       <CommentWriteTextAreaBox
                         minRows={3}
                         placeholder="하고 싶은 말을 전하세요."
-                        {...getFieldProps('comment')}
+                        {...getFieldProps("comment")}
                       />
                     </CommentWriteContentBox>
                   </CommentWriteBox>
@@ -404,13 +428,13 @@ export default function Page() {
                     type="submit"
                     disabled={dirty}
                     style={{
-                      borderRadius: '4px',
+                      borderRadius: "4px",
                       width: 100,
                       height: 40,
-                      float: 'right',
-                      border: '1px solid #f1e0ce',
-                      color: '#f1e0ce',
-                      marginTop: '30px',
+                      float: "right",
+                      border: "1px solid #f1e0ce",
+                      color: "#f1e0ce",
+                      marginTop: "30px",
                     }}
                   >
                     보내기
@@ -422,5 +446,5 @@ export default function Page() {
         </InvitaionWrapper>
       </InvitaionContainer>
     </SnackbarProvider>
-  )
+  );
 }
