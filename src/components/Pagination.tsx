8f63e8f7b895c158c7f8a7b8a59d4a93c8f2a99d
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-07-10 16:36:51 +0900

    Fix: pagination Box style 수정

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -10,14 +10,24 @@ export default function PaginationComponent({
   onChange: any;
 }) {
   return (
-    <Stack sx={{ width: "100%", display: "flex", alignItems: "center" }}>
+    <Stack
+      sx={{
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        position: "absolute",
+        bottom: 5,
+        textAlign: "center",
+        left: "50%",
+        transform: "translateX(-50%)",
+      }}
+    >
       <Pagination
         count={count}
         page={page}
         onChange={onChange}
         size={"small"}
         variant="outlined"
-        color="secondary"
       />
     </Stack>
   );
