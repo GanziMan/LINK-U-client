7e7da79362df15ecc19cafab29e2f92d9a2d8663
GanziMan <qjatn50089@gmail.com>
2024-09-02 23:36:27 +0900

    metaData 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,22 @@ import { Metadata } from 'next'
 import InvitationComponent from './components/invitation'
 
 export const metadata: Metadata = {
-  title: '모바일 청첩장',
+  title: {
+    template: '%s - 모바일 청첩장',
+    default: 'Mobile Invitation',
+  },
+  applicationName: 'Next.js',
+  creator: 'dev Bum',
+  publisher: 'dev Bum',
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
   description: '여자친구와 기념을 위해 만든 모바일 청첩장 사이트입니다.',
   keywords: ['청첩장', '모바일 청첩장', '연인 청첩장'],
   openGraph: {
