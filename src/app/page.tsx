7379e9d995d99f6550261f69bcb19a22248ed812
GanziMan <qjatn50089@gmail.com>
2024-09-02 23:00:23 +0900

    Fix: metaData수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: '모바일 청첩장',
     description: '여자친구와 기념을 위해 만든 모바일 청첩장 사이트입니다.',
-    url: 'https://invitation-n77v0r3g1-bumsukims-projects.vercel.app/',
+    url: 'https://invitation-three-phi.vercel.app/',
     type: 'website',
   },
 }
