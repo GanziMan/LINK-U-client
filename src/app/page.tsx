b56cc4bef895c4e391f3d820e3e7ab1d02c4887c
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-10-18 16:16:50 +0900

    Update: next-seo 설치 및 noIndex false 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 // app/page.tsx
 import { Metadata } from 'next'
 import InvitationComponent from './components/invitation'
+import { NextSeo } from 'next-seo'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - 모바일 청첩장',
+    template: '%s - 모바일 커플 청첩장',
     default: 'Mobile Invitation',
+  },
+
+  verification: {
+    google: 'Zl29et0mZnPrc2Zjwn8RJ3vjPez_FzrfQmrle9GuAbM',
   },
   applicationName: 'Next.js',
   creator: 'dev Bum',
@@ -20,15 +25,19 @@ export const metadata: Metadata = {
     },
   },
   description: '여자친구와 기념을 위해 만든 모바일 청첩장 사이트입니다.',
-  keywords: ['청첩장', '모바일 청첩장', '연인 청첩장'],
+  keywords: ['청첩장', '모바일 청첩장', '연인 청첩장', '커플 청첩장'],
   openGraph: {
-    title: '모바일 청첩장',
+    title: '모바일 커플 청첩장',
     description: '여자친구와 기념을 위해 만든 모바일 청첩장 사이트입니다.',
     url: 'https://invitation-bumsukims-projects.vercel.app',
     type: 'website',
   },
 }
-
 export default function Page() {
-  return <InvitationComponent />
+  return (
+    <>
+      <NextSeo noindex={false} />
+      <InvitationComponent />
+    </>
+  )
 }
