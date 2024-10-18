3ad7b32e0507ce04f23025410784f790d79add43
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-10-18 15:10:07 +0900

    FIx: next-sitemap 활성화에 따른 수동 파일 삭제

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   siteUrl: 'https://invitation-bumsukims-projects.vercel.app', // 여기에 실제 사이트 URL을 넣으세요.
   generateRobotsTxt: true, // robots.txt 파일을 생성합니다.
-  sitemapSize: 7000, // 각 사이트맵 파일의 최대 링크 수입니다.
+  changefreq: 'weekly',
+  priority: 0.7,
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
   //   exclude: ['/secret-page'], // 사이트맵에서 제외할 페이지 경로를 지정합니다.
   // 추가 옵션들을 여기에 설정할 수 있습니다.
 }
