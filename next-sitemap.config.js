3635510bbd86eba2989153286852909af6f7bf00
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-10-18 14:58:30 +0900

    Fix: sitemap url 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://invitation-three-phi.vercel.app/', // 여기에 실제 사이트 URL을 넣으세요.
+  siteUrl: 'https://invitation-bumsukims-projects.vercel.app', // 여기에 실제 사이트 URL을 넣으세요.
   generateRobotsTxt: true, // robots.txt 파일을 생성합니다.
   sitemapSize: 7000, // 각 사이트맵 파일의 최대 링크 수입니다.
   //   exclude: ['/secret-page'], // 사이트맵에서 제외할 페이지 경로를 지정합니다.
