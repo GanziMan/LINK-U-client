580d4a3ff2807ff3ec367361bfa0e537a8e8d257
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-11-04 19:12:28 +0900

    FIx: 사용하지 않는 코드 삭제

--- src/features/invitation/createComment.ts
+++ src/features/invitation/createComment.ts
@@ -1,32 +1,30 @@
-"use server";
+'use server'
 
-import { z } from "zod";
-import { PrismaClient } from "@prisma/client";
-import { createCommentSchema } from "./schema";
-
-export type CreateCommentResponse = Awaited<ReturnType<typeof createComment>>;
+import { z } from 'zod'
+import { PrismaClient } from '@prisma/client'
+import { createCommentSchema } from './schema'
 
 export async function createComment(
   request: z.input<typeof createCommentSchema>
 ) {
-  const validated = await createCommentSchema.safeParseAsync(request);
+  const validated = await createCommentSchema.safeParseAsync(request)
 
-  const prisma = new PrismaClient();
+  const prisma = new PrismaClient()
 
   if (!validated.success) {
     return {
-      code: "VALIDATION ERROR" as const,
+      code: 'VALIDATION ERROR' as const,
       message: validated.error.issues[0].message,
-    };
+    }
   }
 
   const data = await prisma.comments.create({
     data: validated.data,
-  });
+  })
 
   if (data)
     return {
-      status: "200",
-      message: "댓글을 생성하였습니다.",
-    };
+      status: '200',
+      message: '댓글을 생성하였습니다.',
+    }
 }
