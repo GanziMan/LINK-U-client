16197a6b1ac610070f259187c9a1f8d42d7d2682
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-11-04 19:21:40 +0900

    Fix: Request 정의 작업 적용

--- src/features/invitation/getCount.ts
+++ src/features/invitation/getCount.ts
@@ -1,33 +1,33 @@
-"use server";
+'use server'
 
-import { z } from "zod";
-import { PrismaClient } from "@prisma/client";
-import { getCountSchema } from "./schema";
+import { z } from 'zod'
+import { PrismaClient } from '@prisma/client'
+import { GetCountRequest, getCountSchema } from './schema'
 
-export type GetCountResponse = Awaited<ReturnType<typeof getCount>>;
+export type GetCountResponse = Awaited<ReturnType<typeof getCount>>
 
-export async function getCount(request: z.input<typeof getCountSchema>) {
-  const validated = await getCountSchema.safeParseAsync(request);
-  const prisma = new PrismaClient();
+export async function getCount(request: GetCountRequest) {
+  const validated = await getCountSchema.safeParseAsync(request)
+  const prisma = new PrismaClient()
   if (!validated.success) {
     return {
-      code: "VALIDATION ERROR" as const,
+      code: 'VALIDATION ERROR' as const,
       message: validated.error.issues[0].message,
-    };
+    }
   }
 
   const data = await prisma.counts.findUnique({
     where: {
-      id: "1",
+      id: '1',
     },
     select: {
       like_count: true,
     },
-  });
+  })
 
   if (data)
     return {
-      status: "200",
+      status: '200',
       data: data,
-    };
+    }
 }
--- src/features/invitation/updateCount.ts
+++ src/features/invitation/updateCount.ts
@@ -1,34 +1,34 @@
-"use server";
+'use server'
 
-import { z } from "zod";
-import { PrismaClient } from "@prisma/client";
-import { updateCountSchema } from "./schema";
+import { z } from 'zod'
+import { PrismaClient } from '@prisma/client'
+import { UpdateCountRequest, updateCountSchema } from './schema'
 
-export type UpdateCountResponse = Awaited<ReturnType<typeof updateCount>>;
+export type UpdateCountResponse = Awaited<ReturnType<typeof updateCount>>
 
-export async function updateCount(request: z.input<typeof updateCountSchema>) {
-  const validated = await updateCountSchema.safeParseAsync(request);
-  const prisma = new PrismaClient();
+export async function updateCount(request: UpdateCountRequest) {
+  const validated = await updateCountSchema.safeParseAsync(request)
+  const prisma = new PrismaClient()
   if (!validated.success) {
     return {
-      code: "VALIDATION ERROR" as const,
+      code: 'VALIDATION ERROR' as const,
       message: validated.error.issues[0].message,
-    };
+    }
   }
 
   const data = await prisma.counts.update({
     where: {
-      id: "1",
+      id: '1',
     },
     data: {
       like_count: {
         increment: 1,
       },
     },
-  });
+  })
 
   if (data)
     return {
-      status: "200",
-    };
+      status: '200',
+    }
 }
