5ee7ff5b53f332045ab12a484bc73143ad5611de
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-12-03 17:08:45 +0900

    ADD: 계좌폼 추가

---
+++ src/app/components/InvitationAccordionForm.tsx
@@ -0,0 +1,45 @@
+import CommonAccordion from '@/components/Accordion'
+import { Fragment } from 'react'
+import { BrideAccountInfo, GroomAccountInfo } from '../schema/mainPageSchea'
+import { AccountBox, AccountBoxText } from '../styles'
+import Image from 'next/image'
+
+const ACCORDION_DATA = [
+  {
+    AccountInfo: GroomAccountInfo,
+    backgroundColor: 'rgb(240, 243, 246)',
+    gender: '신랑측',
+    genderImageUrl: '/images/icons/groom-icon.svg',
+  },
+  {
+    AccountInfo: BrideAccountInfo,
+    backgroundColor: 'rgb(255, 245, 218)',
+    gender: '신부측',
+    genderImageUrl: '/images/icons/bridge-icon.svg',
+  },
+]
+export default function InvitationAccountForm() {
+  return (
+    <Fragment>
+      <AccountBox>
+        <Image
+          src={'/images/icons/dove-icon.svg'}
+          alt=""
+          width={80}
+          height={70}
+        />
+        <AccountBoxText>마음 전하실 곳</AccountBoxText>
+      </AccountBox>
+      {ACCORDION_DATA.map((data) => {
+        return (
+          <CommonAccordion
+            AccountInfo={data.AccountInfo}
+            backgroundColor={data.backgroundColor}
+            gender={data.gender}
+            genderImageUrl={data.genderImageUrl}
+          />
+        )
+      })}
+    </Fragment>
+  )
+}
