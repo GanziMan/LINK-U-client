01a632f2b7c2da54fc55f9756c9f0788b45d7f53
GanziMan <qjatn50089@gmail.com>
2024-12-10 00:49:50 +0900

    Fix: MUI Pagination style 수정

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,34 +1,44 @@
-import { Stack, Pagination } from "@mui/material";
+import { Stack, Pagination } from '@mui/material'
 
 export default function PaginationComponent({
   count,
   page,
   onChange,
 }: {
-  count: number;
-  page: number;
-  onChange: any;
+  count: number
+  page: number
+  onChange: any
 }) {
   return (
     <Stack
       sx={{
-        width: "100%",
-        display: "flex",
-        alignItems: "center",
-        position: "absolute",
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        position: 'absolute',
+
         bottom: 5,
-        textAlign: "center",
-        left: "50%",
-        transform: "translateX(-50%)",
+        textAlign: 'center',
+        left: '50%',
+        transform: 'translateX(-50%)',
       }}
     >
       <Pagination
         count={count}
         page={page}
         onChange={onChange}
-        size={"small"}
+        size={'small'}
         variant="outlined"
+        sx={{
+          '& .Mui-selected': {
+            background: '#f6fbff',
+          },
+
+          '& .MuiPaginationItem-previousNext': {
+            background: 'white',
+          },
+        }}
       />
     </Stack>
-  );
+  )
 }
