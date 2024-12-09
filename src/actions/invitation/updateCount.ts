a3bae596e5cecc0c259ca12c234f1236e2ace6ec
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-12-09 21:37:56 +0900

    Chore

--- src/actions/invitation/updateCount.ts
+++ src/actions/invitation/updateCount.ts
@@ -1,6 +1,5 @@
 'use server'
 
-import { z } from 'zod'
 import { PrismaClient } from '@prisma/client'
 import { UpdateCountRequest, updateCountSchema } from './schema'
 import { revalidatePath } from 'next/cache'
