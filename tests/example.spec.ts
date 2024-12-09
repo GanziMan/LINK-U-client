91969928200b2453c3f2bdac091dda8bf1c4a761
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-12-09 21:38:07 +0900

    Update: 테스트코드 다시 작성

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,53 +1,42 @@
 import { test, expect } from '@playwright/test'
 
-test.describe('Wedding Invitation Page', () => {
-  // // 테스트 전에 페이지를 로드
-  // test.beforeEach(async ({ page }) => {
-  //   await page.goto('/')
-  // })
-  // // 페이지 타이틀이 올바른지 확인합니다.
-  // test('page title text', async ({ page }) => {
-  //   await expect(page).toHaveTitle('LINK-U')
-  // })
-  // // 페이지 배경음악 재생 테스트
-  // test('background music button test', async ({ page, browserName }) => {
-  //   const backgroundMusicButton = page.locator(
-  //     'button:has-text("배경음악을 눌러주세요.")'
-  //   )
-  //   if (browserName === 'webkit') {
-  //     // await backgroundMusicButton.click()
-  //   } else if (browserName === 'firefox') {
-  //     await page.evaluate(() => {
-  //       const audio = new Audio('/music/wedding-bgm.mp3')
-  //       audio.play()
-  //     })
-  //   } else if (browserName === 'chromium') {
-  //     await page.evaluate(() => {
-  //       const audio = new Audio('/music/wedding-bgm.mp3')
-  //       audio.play()
-  //     })
-  //   }
-  // })
-  // // 페이지 좋아요 테스트
-  // test('like count button test', async ({ page }) => {
-  //   await page.click('#heart-box')
-  // })
-  // // // 방명록 댓글 작성 테스트
-  // // test('comment form test', async ({ page }) => {
-  // //   const initialCommentCount = await page.locator('.comment').count()
-  // //   const testUserName = '테스트 사용자' + Date.now()
-  // //   await page.fill('#name', testUserName)
-  // //   await page.fill('#comment', '이것은 테스트 댓글입니다.')
-  // //   await page.click('button[type="submit"]', { timeout: 3000 })
-  // //   await page.waitForSelector('#comment') // 댓글이 추가될 때까지 대기
-  // //   const finalCommentCount = await page.locator('#comment').count()
-  // //   expect(finalCommentCount).toBe(initialCommentCount + 1)
-  // // })
-  // test('comment form reset test', async ({ page }) => {
-  //   // reset 테스트
-  //   const nameInputValue = await page.inputValue('#name')
-  //   const commentInputValue = await page.inputValue('#comment')
-  //   expect(nameInputValue).toBe('')
-  //   expect(commentInputValue).toBe('')
-  // })
+test.describe('메인 페이지 접근', () => {
+  test('청첩장 좋아요 테스트', async ({ page }) => {
+    // Given: 사용자가 청첩장 페이지 접근
+    await page.goto('/', { waitUntil: 'networkidle' })
+
+    const likeCountSelector = '#like-count'
+    const beforeLikeCount = await page.textContent(likeCountSelector)
+
+    console.log(beforeLikeCount || 0)
+    // When: 사용자가 좋아요 버튼 클릭
+    // await page.click('#heart-box')
+
+    // Then: 좋아요 수가 증가했는지 확인
+    // const afterLikeCount = await page.textContent(likeCountSelector)
+
+    //
+    // expect(afterLikeCountNumber).toBe(beforeLikeCountNumber + 1)
+  })
+
+  //   test('방명록 작성 테스트', async ({ page }) => {
+  //     const testCases = [
+  //       { commentName: '김범수', commentContent: '방명록 작성1' },
+  //     ]
+
+  //     // Given: 사용자가 청첩장 페이지 접근
+  //     await page.goto('/')
+
+  //     // When: 올바른 방명록 작성하고 보내기 버튼을 클릭
+  //     for (const testCase of testCases) {
+  //       await page.fill('#content-name', testCase.commentName)
+  //       await page.fill('#comment-content', testCase.commentContent)
+  //       await page.click('button[type="submit"]')
+
+  //       // Then: 토스트 알림창이 나타나는지 확인
+  //       const toastSelector = '.toast' // 토스트 알림창의 CSS 클래스나 ID
+  //       const toastContent = await page.textContent(toastSelector)
+  //       expect(toastContent).toContain('방명록이 작성되었습니다.')
+  //     }
+  //   })
 })
