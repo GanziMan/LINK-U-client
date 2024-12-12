c64c209038a8c52e3595695fedd37f7a43d73e92
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-12-12 14:59:49 +0900

    Update: CreateComment Response 추가

--- src/actions/invitation/createComment.ts
+++ src/actions/invitation/createComment.ts
@@ -1,27 +1,45 @@
 'use server'
 
 import { PrismaClient } from '@prisma/client'
-import { CreateCommentRequest, createCommentSchema } from './schema'
+import {
+  CreateCommentRequest,
+  CreateCommentResponse,
+  createCommentSchema,
+} from './schema'
+import { commentDataType } from '@/app/schema/mainPageSchea'
 
-export async function createComment(request: CreateCommentRequest) {
+export async function createComment(
+  request: CreateCommentRequest
+): Promise<CreateCommentResponse> {
   const validated = await createCommentSchema.safeParseAsync(request)
 
   const prisma = new PrismaClient()
 
   if (!validated.success) {
     return {
-      code: 'VALIDATION ERROR' as const,
+      status: '400',
       message: validated.error.issues[0].message,
     }
   }
 
-  const createResponse = await prisma.comments.create({
+  const createData = await prisma.comments.create({
     data: validated.data,
   })
 
-  if (createResponse)
+  const formattedCreateData: commentDataType = {
+    name: createData.name,
+    comment: createData.comment,
+    date: createData.createdAt.toISOString(),
+  }
+  if (createData)
     return {
       status: '200',
       message: '댓글을 생성하였습니다.',
+      data: formattedCreateData,
     }
+
+  return {
+    status: '500',
+    message: '댓글 생성에 실패했습니다.',
+  }
 }
